ca6e6948fccda7adc7c5e0560415d627e3af11ae
Aaron Shaver <aaron.shaver@jivesoftware.com>
2017-08-02 14:07:12 -0700

    Beautiful Soup stubbed in

--- job_title_counts.py
+++ job_title_counts.py
@@ -2,11 +2,23 @@ import uuid
 import datetime
 import json
 import boto3
+from bs4 import BeautifulSoup
+import requests
 
 print('Loading function')
 
+
+
 def lambda_handler(event, context):
     client = boto3.client('dynamodb')
+
+    url = 'https://www.indeed.com/jobs?q=%22software+engineer%22&l=Portland%2C+OR'
+    r  = requests.get(url)
+    data = r.text
+    soup = BeautifulSoup(data)
+
+    for link in soup.find_all('a'):
+        print(link.get('href'))
 
     stat_id = str(uuid.uuid4())
     timestamp = str(datetime.datetime.utcnow())
