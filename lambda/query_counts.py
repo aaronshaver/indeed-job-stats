84d9e13582c2fd2e3315023bead73c3ffed4e540
Aaron Shaver <aaron.shaver@jivesoftware.com>
2017-08-02 16:38:47 -0700

    Started moving to proper deployment package, need to pip install bs4 to the subdir and make .zip file

--- lambda/query_counts.py
+++ lambda/query_counts.py
@@ -5,30 +5,30 @@ import boto3
 from bs4 import BeautifulSoup
 import requests
 
-print('Loading function')
 
+def get_query_count(url):
+    r  = requests.get(url)
+    data = r.text
+    soup = BeautifulSoup(data, 'html.parser')
+    div = soup.find('div', {'id': 'searchCount'})
+    return div.text.split()[-1]
 
 
 def lambda_handler(event, context):
     client = boto3.client('dynamodb')
 
-    url = 'https://www.indeed.com/jobs?q=%22software+engineer%22&l=Portland%2C+OR'
-    r  = requests.get(url)
-    data = r.text
-    soup = BeautifulSoup(data)
-
-    for link in soup.find_all('a'):
-        print(link.get('href'))
+    query = '%22software+engineer%22'
+    url = 'https://www.indeed.com/jobs?q=%s&l=Portland%2C+OR', query
 
     stat_id = str(uuid.uuid4())
     timestamp = str(datetime.datetime.utcnow())
-    title = 'dummy title'
-    count = '3'
+    count = get_query_count(url)
 
     client.put_item(TableName='JobTitleCounts', Item={
         'StatId':{'S':stat_id},
         'StatTimestamp':{'S':timestamp},
-        'Title':{'S':title},
+        'Title':{'S':query},
         'Count':{'N':count}
     })
+
     return 'Done'
